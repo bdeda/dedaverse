40c760177879270c6a35713394720e8e2cb1e353
bdeda <ben@dedafx.com>
2024-11-27 17:41:07 -0800

    setup update

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ setup(
     name="dedaverse",
     version="0.1.0",
     packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,    
 )
