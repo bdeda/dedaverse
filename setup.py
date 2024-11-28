6b20924606fa5f6a1c60e4df9f8409be3a57be7b
bdeda <ben@dedafx.com>
2024-11-27 17:42:42 -0800

    fix imports

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import glob
 from setuptools import setup, find_packages
 
 
@@ -6,6 +8,6 @@ setup(
     version="0.1.0",
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob.glob('src/*.py')],
     include_package_data=True,    
 )
